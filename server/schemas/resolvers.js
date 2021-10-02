ddd0ab929cee26e98b4d728fa71038a783769610
Nicholas Wojtalewicz <nicholaswojt@outlook.com>
2021-10-02 17:08:32 -0600

    Started resolvers.js on back end

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,48 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+// import sign token function from auth
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, profile };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { userId, book }) => {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        {
+          $addToSet: { savedBooks: book },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+  },
+};
